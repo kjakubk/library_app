eefe5a12c41c1ccaf094499eeae9717cc9342253
Jakub <jakubkepczynski98@gmail.com>
2026-08-28 19:24:01 +0200

    fix: Restore Paginator import

--- library/views.py
+++ library/views.py
@@ -10,6 +10,7 @@ from django.db.models import Q, Count
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.core.files.base import ContentFile
+from django.core.paginator import Paginator
 from django.http import JsonResponse, HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
